68738f2cc92a9db3cdabfc84a5eddf5549600425
tfh <tfh@mit.edu>
2020-11-25 17:42:02 -0500

    prettier footer

--- js/footer.js
+++ js/footer.js
@@ -1,12 +1,22 @@
-document.getElementById("footer").innerHTML =
-"<div class='footer'>" +
-"<a href='https://www.instagram.com/dylanjhenderson'>" +
-"<img class='footer_img' src='../media/insta.png' width=22 height=22/>" +
-"</a> &nbsp;" +
-"<a href='mailto:dylanhendersonj@gmail.com'>" +
-"<img class='footer_img' src='../media/mail.png' width=22 height=22/>" +
-"</a> &nbsp;" +
-"<a href='https://vimeo.com/user81614983'>" +
-"<img class='footer_img' src='../media/vimeo.png' width=22 height=22/>" +
-"</a> &nbsp;" +
-"</div>";
+// Create a footer element
+var footer = document.createElement('footer');
+footer.className = "footer";
+document.body.appendChild(footer);
+
+// Add icons
+function add_icon(f, href, img_src) {
+  var img = document.createElement('img');
+  img.src = img_src;
+  img.width = 22;
+  img.height = 22;
+  img.className = "footer_img";
+  var link = document.createElement('a');
+  link.href = href;
+  link.appendChild(img);
+  f.appendChild(link);
+  var br = document.createElement("br");
+  f.appendChild(br);
+}
+add_icon(footer, "https://www.instagram.com/dylanjhenderson", "../media/insta.png");
+add_icon(footer, "mailto:dylanjhendersonj@gmail.com", "../media/mail.png");
+add_icon(footer, "https://vimeo.com/user81614983", "../media/vimeo.png");
