89d7d68f970a985e937e5630ff4ebb4cbcbf8b18
tfh <tfh@mit.edu>
2020-11-25 10:56:17 -0500

    Header and footer include

---
+++ navigation.js
@@ -0,0 +1,10 @@
+document.getElementById("navigation").innerHTML =
+"<a href='../index.html'>" +
+"<h1>DYLAN HENDERSON</h1>" +
+"</a>" +
+"<h2>" +
+"<a href='../Animation/index.html'>Animation</a> &nbsp;" +
+"<a href='../Fabrication/index.html'>Fabrication</a> &nbsp;" +
+"<a href='../Films/index.html'>Films</a> &nbsp;" +
+"<a href='../About/index.html'>About</a> &nbsp;" +
+"</h2>";
